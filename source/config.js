ca11e1b21a81eca43d7982284cb7986eaaf8ea02
armandojes <armandodejesus@gmail.com>
2020-11-09 11:51:22 -0600

    update

--- source/config.js
+++ source/config.js
@@ -1,44 +1,44 @@
-export const secret = '' // secret for security
+export const secret = 'helloforyou'
 export const isProduction = ENV === 'production'
 
 const firebaseConfigProduction = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyAnYHA114ubzMjUDND3V5J852bfM1cPmUY',
+  authDomain: 'arts-bfab3.firebaseapp.com',
+  databaseURL: 'https://arts-bfab3.firebaseio.com',
+  projectId: 'arts-bfab3',
+  storageBucket: 'arts-bfab3.appspot.com',
+  messagingSenderId: '804609683943',
+  appId: '1:804609683943:web:893f59b6de6d63e7c40d42'
 }
 const firebaseConfigDevelop = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyCKNcHonL_1Wv-feCa6Uaviop8BQJgW5JE',
+  authDomain: 'artesaniasybordados-develop.firebaseapp.com',
+  databaseURL: 'https://artesaniasybordados-develop.firebaseio.com',
+  projectId: 'artesaniasybordados-develop',
+  storageBucket: 'artesaniasybordados-develop.appspot.com',
+  messagingSenderId: '25365981751',
+  appId: '1:25365981751:web:f21bc8972c40e7fd8fadb2'
 }
 
 export const domains = {
-  production: '' // url production project
+  production: 'https://artesaniasybordados.com.mx'
 }
 
 export const firebaseConfig = isProduction ? firebaseConfigProduction : firebaseConfigDevelop
 
 export const paypal = {
-  token: '',
-  prodToken: ''
+  token: 'AaSWKyX_04IYi1ka8A_Qxr-xddBNxbn_K0fzZukxjYHN1BR01dv6wykgLV_zoBL16adglxM6ixmC2jps',
+  prodToken: 'AQflwcCafKBrBiRwQFRPct8BESbYy4spJRt97pj47crYetM9kBkQHZjKZU-uvZFsl1hCmaVaNt5UpUfg'
 }
 
 export const mercadopago = {
-  PublicKey: ''
+  PublicKey: 'TEST-55009628-46e2-4102-ad7a-596b5922b9af'
 }
 
 export const fiServe = {
-  apiKey: '',
-  apiSecret: '',
-  url: ''
+  apiKey: 'y8mlwZApi8UmtJPQGdOCiSmOwjDBnERA',
+  apiSecret: 'rIoBSymVqIEQ2rzJ',
+  url: 'https://cert.api.firstdata.com/gateway/v2'
 }
 
 export const limitPictures = 15
